f7c848a81b8365fc56d056148ab7538ccac7de49
Jesús Antonio Sarco Giannini <jesus.antonio.sarco@gmail.com>
2020-08-12 18:17:18 +0200

    JMS Configured - closes #7

--- target/generated-sources/annotations/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-08-11T14:41:39+0200",
+    date = "2020-08-12T18:04:34+0200",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.8 (Oracle Corporation)"
 )
 @Component
