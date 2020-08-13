7311440816ab0ee6cd09d024a203da667066bd53
Jesús Antonio Sarco Giannini <jesus.antonio.sarco@gmail.com>
2020-08-13 02:18:01 +0200

    Implement New Inventory Listener - closes #8

---
+++ src/main/java/guru/sfg/common/events/BeerEvent.java
@@ -0,0 +1,22 @@
+package guru.sfg.common.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * Created by jantonio on 12/08/2020.
+ */
+@Data
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+public class BeerEvent implements Serializable {
+
+    static final long serialVersionUID = -3248857530373176819L;
+
+    private BeerDto beerDto;
+}
---
+++ src/main/java/guru/sfg/common/events/NewInventoryEvent.java
@@ -0,0 +1,14 @@
+package guru.sfg.common.events;
+
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by jantonio on 12/08/2020.
+ */
+@NoArgsConstructor
+public class NewInventoryEvent extends BeerEvent {
+
+    public NewInventoryEvent(BeerDto beerDto) {
+        super(beerDto);
+    }
+}
